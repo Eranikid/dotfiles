64f03420ed93c6ce47fa7fde0ab370ae64f9afc8
Eranikid <pavlushindmitry@gmail.com>
2020-08-12 20:56:16 +0300

    desired svcs

--- .local/bin/install.sh
+++ .local/bin/install.sh
@@ -13,6 +13,10 @@ function prompt() {
     echo
 }
 
+function get_enabled_systemd_services {
+    systemctl list-unit-files | tr -s [:blank:] | cut -d' ' -f1,2 | grep 'enabled$' | cut -d' ' -f1
+}
+
 installed_pkgs=$(pacman -Qettq)
 desired_pkgs=$(sort ~/.local/share/desired_pkgs)
 
@@ -33,3 +37,11 @@ if ! [ -z "$pkgs_to_delete" ]; then
 	[ $prompt_result == "y" ] && sudo pacman --noconfirm -Rscn $pkgs_to_delete || echo "Skipping package removal..."
 fi
 
+enabled_svcs=$(get_enabled_systemd_services)
+desired_svcs=$(sort ~/.local/share/desired_svcs)
+
+svcs_to_enable=$(comm -13 <(echo $enabled_svcs | tr ' ' '\n') <(echo $desired_svcs | tr ' ' '\n'))
+svcs_to_disable=$(comm -23 <(echo $enabled_svcs | tr ' ' '\n') <(echo $desired_svcs | tr ' ' '\n'))
+
+echo $svcs_to_enable
+echo $svcs_to_disable
