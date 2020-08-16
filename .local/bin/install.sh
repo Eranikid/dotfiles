3a969848f8fe25ba80156d3bb62190b342d6341a
Eranikid <pavlushindmitry@gmail.com>
2020-08-16 14:17:08 +0300

    Add rofi-dmenu

--- .local/bin/install.sh
+++ .local/bin/install.sh
@@ -17,7 +17,7 @@ function get_enabled_systemd_services {
     systemctl list-unit-files | tr -s [:blank:] | cut -d' ' -f1,2 | grep 'enabled$' | cut -d' ' -f1
 }
 
-installed_pkgs=$(pacman -Qettq)
+installed_pkgs=$(yay -Qettq)
 desired_pkgs=$(sort ~/.local/share/desired_pkgs)
 
 pkgs_to_install=$(comm -13 <(echo $installed_pkgs | tr ' ' '\n') <(echo $desired_pkgs | tr ' ' '\n'))
@@ -27,14 +27,14 @@ if ! [ -z "$pkgs_to_install" ]; then
 	echo "Need to install following packages:"
 	echo "$pkgs_to_install"
 	prompt "Proceed?"
-	[ $prompt_result == "y" ] && sudo pacman --noconfirm -S $pkgs_to_install || echo "Skipping package install..."	
+	[ $prompt_result == "y" ] && yay --noconfirm -S $pkgs_to_install || echo "Skipping package install..."	
 fi
 
 if ! [ -z "$pkgs_to_delete" ]; then
 	echo "Need to delete following packages:"
 	echo "$pkgs_to_delete"
 	prompt "Proceed?"
-	[ $prompt_result == "y" ] && sudo pacman --noconfirm -Rscn $pkgs_to_delete || echo "Skipping package removal..."
+	[ $prompt_result == "y" ] && yay --noconfirm -Rscn $pkgs_to_delete || echo "Skipping package removal..."
 fi
 
 enabled_svcs=$(get_enabled_systemd_services | sort)
